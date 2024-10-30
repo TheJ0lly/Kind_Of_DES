27272d7057820c48e3da60be94d29b695f67593c
TheJ0lly <eusicumine7@gmail.com>
2024-10-30 20:47:29 +0200

    Fixed memory-leak. Added argc, argv.

--- src/main.c
+++ src/main.c
@@ -3,9 +3,11 @@
 #include <string.h>
 
 
-int main() {
+int main(int argc, char **argv) {
     char *h = malloc(sizeof(char) * strlen("Hello, World"));
     h = "Hello, World";
 
     printf("%s\n", h);
+
+    free(h);
 }
